0faf2d6be2f3b9a60272d696fe1bd47909993740
samara <samduarte1234@gmail.com>
2020-04-09 13:52:56 -0300

    Exercicio 2

--- Aula1/src/app/eventos/eventos.component.ts
+++ Aula1/src/app/eventos/eventos.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
+  evento = {name: 'PHP Conference',
+   date: '6/30/2020',
+   time: '10am',
+   location: {address: 'UniBH ', city: 'Belo Horizonte', country: 'Brasil'}};
+
   constructor() { }
 
   ngOnInit(): void {
